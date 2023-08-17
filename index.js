5aaa460f1495e4020ccb122fc344d86580a0c578
luan <los8@aluno.ifal.edu.br>
2023-08-17 14:15:40 -0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function registrarDespesa(categoria) {
-    const valor = parseFloat(document.getElementById('entradaValor').value);
-    if (!isNaN(valor)) {
-        const historicoLista = document.getElementById('historicoLista');
-        const listItem = document.createElement('li');
-        listItem.textContent = `${categoria}: R$ ${valor.toFixed(2)}`;
-        historicoLista.appendChild(listItem);
-        document.getElementById('entradaValor').value = ''; // Limpa o campo após o registro
-    }
+function clickTransporte(letra) {
+    console.log(letra);
+}
+
+function clickMercado(letra) {
+  console.log(letra);
+}
+
+function clickOutros(letra) {
+  console.log(letra);
 }

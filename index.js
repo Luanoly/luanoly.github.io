9b2b9bc952376aa5759428f321b3e9d0913a7790
luan <los8@aluno.ifal.edu.br>
2023-08-17 13:43:43 -0300

    botão funcionando no console

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 //     button.addEventListener('click', function() {
 //       console.log('Botão Transporte clicado');
 //     });
+function clickTransporte(letra) {
+  console.log(letra);
+}
+
+function clickMercado(letra) {
+  console.log(letra);
+}
+
+function clickOutros(letra) {
+  console.log(letra);
+}
